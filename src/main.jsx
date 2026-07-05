061f90187a5633093c6a2efad717c966ac46e319
Miguel Aviles <avilesmiguel1996@yahoo.com>
2026-07-04 20:08:50 -0500

    fix vercel deployment

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/Movie-react">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
